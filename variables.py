e680dcbc69867b15ba56e4b19c8f12257ab9f9e7
Brian Snitzer <brian@snitzer.org>
2019-07-03 15:26:35 -0400

    More variables

--- variables.py
+++ variables.py
@@ -1,3 +1,7 @@
+# Brian Snitzer
+# snitzer@us.ibm.
+
+
 number = 1
 print(number)
 print(type(number))
@@ -5,3 +9,14 @@ print(type(number))
 letter = "B"
 print(letter)
 print(type(letter))
+
+print("Variables in section 3")
+
+first_number = 1+1+5
+print(first_number)
+
+second_number = 105+10
+print(second_number)
+
+total = first_number + second_number
+print("TOTAL:", total)
