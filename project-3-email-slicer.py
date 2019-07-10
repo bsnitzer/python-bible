d5616fc1a472771c4303a246b18ea7b63dc018b6
Brian Snitzer <brian@snitzer.org>
2019-07-10 14:12:42 -0400

    updated slicer

--- project-3-email-slicer.py
+++ project-3-email-slicer.py
@@ -2,19 +2,18 @@
 # July 10, 2019
 # Exploring string methods
 
-string1 = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+# Get user email
+email = input("What is your email address?:").strip()
 
-string1
-print("-----")
-print(string1.count("e"))
-print("-----")
-print("Lorem:", string1.count("Lorem"))
-print("-----")
-print(string1.lower())
-print("-----")
-print(string1.upper())
-print("-----")
-print(string1.title())
-print("-----")
-print(string1.isalpha())
-print("-----")
+# Slice out user user_name
+user = email[:email.index("@")]
+
+# slice out domain
+domain = email[email.index("@") + 1:]
+
+# format message
+
+message = "Your user name is {} and your domain name is {}".format(user,domain)
+# display output
+
+print(message)
