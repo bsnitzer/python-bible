d9a48e7a186ba5882f4a99594c6c9b1f254ade1a
Brian Snitzer <brian@snitzer.org>
2019-07-10 16:04:33 -0400

    Section 6 stuff

---
+++ logic.py
@@ -0,0 +1,19 @@
+# Brian Snitzer
+# July 2019
+
+b = True
+c = False
+print(type(b))
+print(2 > 3)
+print("2 = 3? ", 2 == 3)
+
+print("IF STATEMENTS \
+        ")
+
+number = int(input("Give me a number: "))
+if (number < 5):
+    print("That's less than 5")
+elif (number == 5):
+    print("that's 5")
+else:
+    print("that's more than 5")
