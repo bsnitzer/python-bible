72b6369816a3462773c871b8a9a0f03eafed20b8
Brian Snitzer <brian@snitzer.org>
2019-07-10 12:55:59 -0400

    added project 3

---
+++ project-3-email-slicer.py
@@ -0,0 +1,20 @@
+# Brian Snitzer
+# July 10, 2019
+# Exploring string methods
+
+string1 = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
+string1
+print("-----")
+print(string1.count("e"))
+print("-----")
+print("Lorem:", string1.count("Lorem"))
+print("-----")
+print(string1.lower())
+print("-----")
+print(string1.upper())
+print("-----")
+print(string1.title())
+print("-----")
+print(string1.isalpha())
+print("-----")
