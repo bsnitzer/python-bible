70d341c52f35b9efd86727333fadfcf30198ed35
Brian Snitzer <brian@snitzer.org>
2019-07-12 15:47:54 -0400

    Added Travis project 4

--- project-4-travis.py
+++ project-4-travis.py
@@ -14,8 +14,13 @@ while True:
         remove = input("Would you like to be removed?  (y/n)").strip().lower()
         if remove == "y":
             known_users.remove(name)
+        elif remove == "n":
+            print("Okey Dokey")
+
     else:
-        print("Hmm, I don't think I've met you yet {}.")
+        print("Hmm, I don't think I've met you yet {}.".format(name))
         add_me = input("Would you like to be added? (y/n)").strip().lower()
         if add_me == "y":
             known_users.append(name)
+        elif add_me == "n":
+            print("Nice to meet you anyway.")
